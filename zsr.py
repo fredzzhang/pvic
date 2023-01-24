088accb84de50b7a69bed1ba6ec52ea34994d515
fred@gcr <v-zhenzha@microsoft.com>
2023-01-24 03:12:51 +0000

    fix a few bugs
    Signed-off-by: fred@gcr <v-zhenzha@microsoft.com>

--- zsr.py
+++ zsr.py
@@ -17,7 +17,7 @@ from tqdm import tqdm
 from hicodet.hicodet import HICODet
 
 def construct_prompt(s):
-    v, o = s.split("")
+    v, o = s.split()
     if v == "no_interaction":
         return f"a photo of a person and a {o}"
     else:
@@ -34,34 +34,34 @@ def main(args):
         labels[t["hoi"]] = 1
         return labels
     dataset = HICODet(
-        os.path.join(args.dr, f"images/{args.p}"),
-        os.path.join(args.dr, f"instances_{args.p}.json"),
+        os.path.join(args.data_root, f"hico_20160224_det/images/{args.partition}"),
+        os.path.join(args.data_root, f"instances_{args.partition}.json"),
         transform=preprocess, target_transform=target_transform
     )
     dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=args.bs,
-        num_workers=args.nw, pin_memory=use_cuda,
+        dataset, batch_size=args.batch_size,
+        num_workers=args.num_workers, pin_memory=use_cuda,
         
     )
 
-    text = clip.tokenize(dataset.interaction).to(device)
+    text = clip.tokenize([construct_prompt(p) for p in dataset.interactions]).to(device)
     text_features = model.encode_text(text)
     text_features /= text_features.norm(dim=1, keepdim=True)
 
     ap = pocket.utils.AveragePrecisionMeter()
     for images, targets in tqdm(dataloader):
-        image_features = model.encode(images.to(device))
+        image_features = model.encode_image(images.to(device))
         image_features /= image_features.norm(dim=1, keepdim=True)
 
-        logits = image_features @ text_features.t()
-        ap.append(logits, targets)
+        cos = image_features @ text_features.t()
+        ap.append(cos, targets)
 
     print(ap.eval().mean())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--data-root", "-dr", type=str, default="hicodet")
-    parser.add_argument("--partition", "-p", type=str, default="train2015")
+    parser.add_argument("--partition", "-p", type=str, default="test2015")
     parser.add_argument("--batch-size", "-bs", type=int, default=128)
     parser.add_argument("--num-workers", "-nw", type=int, default=8)
     args = parser.parse_args()

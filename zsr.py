d51a9c1f908c658ee2b7471c6268e063cb317409
fred@gcr <v-zhenzha@microsoft.com>
2023-01-25 06:51:44 +0000

    separte map computation; add function for network surgery on clip
    Signed-off-by: fred@gcr <v-zhenzha@microsoft.com>

--- zsr.py
+++ zsr.py
@@ -14,8 +14,40 @@ import torch
 import pocket
 import argparse
 
+from torchvision.transforms import (
+    Compose, Resize, CenterCrop,
+    ToTensor, Normalize,
+    InterpolationMode
+)
+
 from tqdm import tqdm
 from hicodet.hicodet import HICODet
+
+def _convert_image_to_rgb(image):
+    return image.convert("RGB")
+def _transform(n_px):
+    return Compose([
+        Resize(n_px, interpolation=InterpolationMode.BICUBIC),
+        CenterCrop(n_px),
+        _convert_image_to_rgb,
+        ToTensor(),
+        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+    ])
+def clip_forward(ctx, x):
+    x = x.type(ctx.visual.conv1.weight.dtype)
+    x = ctx.visual.relu1(ctx.visual.bn1(ctx.visual.conv1(x)))
+    x = ctx.visual.relu2(ctx.visual.bn2(ctx.visual.conv2(x)))
+    x = ctx.visual.relu3(ctx.visual.bn3(ctx.visual.conv3(x)))
+    x = ctx.visual.avgpool(x)
+
+    x = ctx.visual.layer1(x)
+    x = ctx.visual.layer2(x)
+    x = ctx.visual.layer3(x)
+    x = ctx.visual.layer4(x)
+    x = Resize(7, InterpolationMode.BICUBIC)(x)
+    x = ctx.visual.attnpool(x)
+
+    return x
 
 @torch.no_grad()
 def main(args):
@@ -30,12 +62,11 @@ def main(args):
     dataset = HICODet(
         os.path.join(args.data_root, f"hico_20160224_det/images/{args.partition}"),
         os.path.join(args.data_root, f"instances_{args.partition}.json"),
-        transform=preprocess, target_transform=target_transform
+        transform=_transform(224), target_transform=target_transform
     )
     dataloader = torch.utils.data.DataLoader(
         dataset, batch_size=args.batch_size,
         num_workers=args.num_workers, pin_memory=use_cuda,
-        
     )
 
     with open("hicodet/prompts.json", "r") as f:
@@ -44,19 +75,25 @@ def main(args):
     text_features = model.encode_text(text)
     text_features /= text_features.norm(dim=1, keepdim=True)
 
+    if args.map:
+        eval_zs_recognition(dataloader, model, text_features, device)
+
+def eval_zs_recognition(dataloader, model, class_embeddings, device):
+    """Evaluate the zero-shot interaction recognition mAP"""
     ap = pocket.utils.AveragePrecisionMeter()
     for images, targets in tqdm(dataloader):
-        image_features = model.encode_image(images.to(device))
+        # image_features = model.encode_image(images.to(device))
+        image_features = clip_forward(model, images.to(device))
         image_features /= image_features.norm(dim=1, keepdim=True)
 
-        cos = image_features @ text_features.t()
+        cos = image_features @ class_embeddings.t()
         ap.append(cos, targets)
 
     ap = ap.eval()
     print(
         f"The mAP is {ap.mean():.4f},"
-        f" rare: {ap[dataset.rare].mean():.4f},"
-        f" none-rare: {ap[dataset.non_rare].mean():.4f}"
+        f" rare: {ap[dataloader.dataset.rare].mean():.4f},"
+        f" none-rare: {ap[dataloader.dataset.non_rare].mean():.4f}"
     )
 
 if __name__ == "__main__":
@@ -65,5 +102,6 @@ if __name__ == "__main__":
     parser.add_argument("--partition", "-p", type=str, default="test2015")
     parser.add_argument("--batch-size", "-bs", type=int, default=128)
     parser.add_argument("--num-workers", "-nw", type=int, default=8)
+    parser.add_argument("--map", action="store_true", default=False)
     args = parser.parse_args()
     main(args)

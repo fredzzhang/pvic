e8cb4ff5a4218a2d8b7b1c6ec3175b91cb1cfc45
fred@gcr <frederic.zhang@anu.edu.au>
2023-01-29 02:37:27 +0000

    adapt the transformer decoder implementation
    Signed-off-by: fred@gcr <frederic.zhang@anu.edu.au>

--- interaction_head.py
+++ interaction_head.py
@@ -178,6 +178,23 @@ class ModifiedEncoder(nn.Module):
 class TransformerDecoderLayer(nn.Module):
 
     def __init__(self, q_size, kv_size, num_heads, ffn_interm_size=2048, dropout=0.1):
+        """
+        Transformer decoder layer, adapted from DETR codebase by Facebook Research
+        https://github.com/facebookresearch/detr/blob/main/models/transformer.py#L187
+
+        Parameters:
+        -----------
+        q_size: int
+            Dimension of the interaction queries.
+        kv_size: int
+            Dimension of the image features.
+        num_heads: int
+            Number of heads used in multihead attention.
+        ffn_interm_size: int, default: 2048
+            The intermediate size in the feedforward network.
+        dropout: float, default: 0.1
+            Dropout percentage used during training.
+        """
         super().__init__()
         self.q_size = q_size
         self.kv_size = kv_size
@@ -207,28 +224,71 @@ class TransformerDecoderLayer(nn.Module):
     def with_pos_embed(self, tensor, pos: Optional[Tensor]):
         return tensor if pos is None else tensor + pos
 
-    def forward(self, tgt, memory,
-                     tgt_mask: Optional[Tensor] = None,
-                     memory_mask: Optional[Tensor] = None,
-                     tgt_key_padding_mask: Optional[Tensor] = None,
-                     memory_key_padding_mask: Optional[Tensor] = None,
-                     pos: Optional[Tensor] = None,
-                     query_pos: Optional[Tensor] = None):
-        q = k = self.with_pos_embed(tgt, query_pos)
-        tgt2 = self.q_attn(q, k, value=tgt, attn_mask=tgt_mask,
-                              key_padding_mask=tgt_key_padding_mask)[0]
-        tgt = tgt + self.dp1(tgt2)
-        tgt = self.ln1(tgt)
-        tgt2 = self.qk_attn(query=self.with_pos_embed(tgt, query_pos),
-                                key=self.with_pos_embed(memory, pos),
-                                value=memory, attn_mask=memory_mask,
-                                key_padding_mask=memory_key_padding_mask)[0]
-        tgt = tgt + self.dp2(tgt2)
-        tgt = self.ln2(tgt)
-        tgt2 = self.ffn(tgt)
-        tgt = tgt + self.dp3(tgt2)
-        tgt = self.ln3(tgt)
-        return tgt
+    def forward(self, queries, features,
+            q_mask: Optional[Tensor] = None,
+            kv_mask: Optional[Tensor] = None,
+            q_padding_mask: Optional[Tensor] = None,
+            kv_padding_mask: Optional[Tensor] = None,
+            q_pos: Optional[Tensor] = None,
+            kv_pos: Optional[Tensor] = None,
+            return_q_attn_weights: Optional[bool] = False,
+            return_qk_attn_weights: Optional[bool] = False
+        ):
+        """
+        Parameters:
+        -----------
+        queries: Tensor
+            Interaction queries of size (B, N, K).
+        features: Tensor
+            Image features of size (B, C, H, W).
+        q_mask: Tensor, default: None
+            Attention mask to be applied during the self attention of queries.
+        kv_mask: Tensor, default: None
+            Attention mask to be applied during the cross attention from image
+            features to interaction queries.
+        q_padding_mask: Tensor, default: None
+            Padding mask for interaction queries of size (B, N). Values of `True`
+            indicate the corresponding query was padded and to be ignored.
+        kv_padding_mask: Tensor, default: None
+            Padding mask for image features of size (B, HW).
+        q_pos: Tensor, default: None
+            Positional encodings for the interaction queries.
+        kv_pos: Tensor, default: None
+            Positional encodings for the image features.
+        return_q_attn_weights: bool, default: False
+            If `True`, return the self attention weights.
+        return_qk_attn_weights: bool, default: False
+            If `True`, return the cross attention weights.
+
+        Returns:
+        --------
+        outputs: list
+            A list with the order [queries, q_attn_w, qk_attn_w], if both weights are
+            to be returned.
+        """
+        q = k = self.with_pos_embed(queries, q_pos)
+        # Perform self attention amongst queries
+        q_attn, q_attn_weights = self.q_attn(
+            q, k, value=queries, attn_mask=q_mask,
+            key_padding_mask=q_padding_mask
+        )
+        queries = self.ln1(queries + self.dp1(q_attn))
+        # Perform cross attention from memory features to queries
+        qk_attn, qk_attn_weights = self.qk_attn(
+            query=self.with_pos_embed(queries, q_pos),
+            key=self.with_pos_embed(features, kv_pos),
+            value=features, attn_mask=kv_mask,
+            key_padding_mask=kv_padding_mask
+        )
+        queries = self.ln2(queries + self.dp2(qk_attn))
+        queries = self.ln3(queries + self.dp3(self.ffn(queries)))
+
+        outputs = [queries,]
+        if return_q_attn_weights:
+            outputs.append(q_attn_weights)
+        if return_qk_attn_weights:
+            outputs.append(qk_attn_weights)
+        return outputs
 
 class TransformerDecoder(nn.Module):
 
@@ -239,36 +299,49 @@ class TransformerDecoder(nn.Module):
         self.norm = nn.LayerNorm(decoder_layer.q_size)
         self.return_intermediate = return_intermediate
 
-    def forward(self, tgt, memory,
-                tgt_mask: Optional[Tensor] = None,
-                memory_mask: Optional[Tensor] = None,
-                tgt_key_padding_mask: Optional[Tensor] = None,
-                memory_key_padding_mask: Optional[Tensor] = None,
-                pos: Optional[Tensor] = None,
-                query_pos: Optional[Tensor] = None):
-        output = tgt
+    def forward(self, queries, features,
+            q_mask: Optional[Tensor] = None,
+            kv_mask: Optional[Tensor] = None,
+            q_padding_mask: Optional[Tensor] = None,
+            kv_padding_mask: Optional[Tensor] = None,
+            q_pos: Optional[Tensor] = None,
+            kv_pos: Optional[Tensor] = None,
+            return_q_attn_weights: Optional[bool] = False,
+            return_qk_attn_weights: Optional[bool] = False
+        ):
 
+        output = queries
         intermediate = []
-
+        q_attn_w = []
+        qk_attn_w = []
         for layer in self.layers:
-            output = layer(output, memory, tgt_mask=tgt_mask,
-                           memory_mask=memory_mask,
-                           tgt_key_padding_mask=tgt_key_padding_mask,
-                           memory_key_padding_mask=memory_key_padding_mask,
-                           pos=pos, query_pos=query_pos)
+            output = layer(
+                output, features,
+                q_mask=q_mask, kv_mask=kv_mask,
+                q_padding_mask=q_padding_mask,
+                kv_padding_mask=kv_padding_mask,
+                q_pos=q_pos, kv_pos=kv_pos,
+                return_q_attn_weights=return_q_attn_weights,
+                return_qk_attn_weights=return_qk_attn_weights
+            )
             if self.return_intermediate:
-                intermediate.append(self.norm(output))
-
-        if self.norm is not None:
-            output = self.norm(output)
-            if self.return_intermediate:
-                intermediate.pop()
-                intermediate.append(output)
+                intermediate.append(self.norm(output[0]))
+            if return_q_attn_weights:
+                q_attn_w.append(output[1])
+            if return_qk_attn_weights:
+                qk_attn_w.append(output[2])
 
         if self.return_intermediate:
             return torch.stack(intermediate)
 
-        return output.unsqueeze(0)
+        outputs = [intermediate[-1].unsqueeze(0),]
+        if return_q_attn_weights:
+            q_attn_w = torch.stack(q_attn_w)
+            outputs.append(q_attn_w)
+        if return_qk_attn_weights:
+            qk_attn_w = torch.stack(qk_attn_w)
+            outputs.append(qk_attn_w)
+        return outputs
 
 class InteractionHead(nn.Module):
     """

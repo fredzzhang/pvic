89fde99a2fc5bedf048b1af7527514d2313fa890
fred@menlopark <frederic.zhang@anu.edu.au>
2023-02-15 03:33:08 +0000

    add deformable attention for decoder
    Signed-off-by: fred@menlopark <frederic.zhang@anu.edu.au>

--- upt.py
+++ upt.py
@@ -125,184 +125,6 @@ class HumanObjectMatcher(nn.Module):
 
         return ho_queries, paired_indices, prior_scores, object_types
 
-class TransformerDecoderLayer(nn.Module):
-
-    def __init__(self, q_dim, kv_dim, num_heads, ffn_interm_dim, dropout=0.1):
-        """
-        Transformer decoder layer, adapted from DETR codebase by Facebook Research
-        https://github.com/facebookresearch/detr/blob/main/models/transformer.py#L187
-
-        Parameters:
-        -----------
-        q_dim: int
-            Dimension of the interaction queries.
-        kv_dim: int
-            Dimension of the image features.
-        num_heads: int
-            Number of heads used in multihead attention.
-        ffn_interm_dim: int
-            Dimension of the intermediate representation in the feedforward network.
-        dropout: float, default: 0.1
-            Dropout percentage used during training.
-        """
-        super().__init__()
-        self.q_dim = q_dim
-        self.kv_dim = kv_dim
-        self.num_heads = num_heads
-        self.dropout = dropout
-        self.ffn_interm_dim = ffn_interm_dim
-
-        self.q_attn = nn.MultiheadAttention(q_dim, num_heads, dropout=dropout)
-        self.qk_attn = nn.MultiheadAttention(
-            q_dim, num_heads,
-            kdim=kv_dim, vdim=kv_dim,
-            dropout=dropout
-        )
-        self.ffn = nn.Sequential(
-            nn.Linear(q_dim, ffn_interm_dim),
-            nn.ReLU(),
-            nn.Dropout(dropout),
-            nn.Linear(ffn_interm_dim, q_dim)
-        )
-        self.ln1 = nn.LayerNorm(q_dim)
-        self.ln2 = nn.LayerNorm(q_dim)
-        self.ln3 = nn.LayerNorm(q_dim)
-        self.dp1 = nn.Dropout(dropout)
-        self.dp2 = nn.Dropout(dropout)
-        self.dp3 = nn.Dropout(dropout)
-
-    def with_pos_embed(self, tensor, pos: Optional[Tensor]):
-        return tensor if pos is None else tensor + pos
-
-    def forward(self, queries, features,
-            q_mask: Optional[Tensor] = None,
-            kv_mask: Optional[Tensor] = None,
-            q_padding_mask: Optional[Tensor] = None,
-            kv_padding_mask: Optional[Tensor] = None,
-            q_pos: Optional[Tensor] = None,
-            kv_pos: Optional[Tensor] = None,
-            return_q_attn_weights: Optional[bool] = False,
-            return_qk_attn_weights: Optional[bool] = False
-        ):
-        """
-        Parameters:
-        -----------
-        queries: Tensor
-            Interaction queries of size (B, N, K).
-        features: Tensor
-            Image features of size (B, C, H, W).
-        q_mask: Tensor, default: None
-            Attention mask to be applied during the self attention of queries.
-        kv_mask: Tensor, default: None
-            Attention mask to be applied during the cross attention from image
-            features to interaction queries.
-        q_padding_mask: Tensor, default: None
-            Padding mask for interaction queries of size (B, N). Values of `True`
-            indicate the corresponding query was padded and to be ignored.
-        kv_padding_mask: Tensor, default: None
-            Padding mask for image features of size (B, HW).
-        q_pos: Tensor, default: None
-            Positional encodings for the interaction queries.
-        kv_pos: Tensor, default: None
-            Positional encodings for the image features.
-        return_q_attn_weights: bool, default: False
-            If `True`, return the self attention weights.
-        return_qk_attn_weights: bool, default: False
-            If `True`, return the cross attention weights.
-
-        Returns:
-        --------
-        outputs: list
-            A list with the order [queries, q_attn_w, qk_attn_w], if both weights are
-            to be returned.
-        """
-        q = k = self.with_pos_embed(queries, q_pos)
-        # Perform self attention amongst queries
-        q_attn, q_attn_weights = self.q_attn(
-            q, k, value=queries, attn_mask=q_mask,
-            key_padding_mask=q_padding_mask
-        )
-        queries = self.ln1(queries + self.dp1(q_attn))
-        # Perform cross attention from memory features to queries
-        qk_attn, qk_attn_weights = self.qk_attn(
-            query=self.with_pos_embed(queries, q_pos),
-            key=self.with_pos_embed(features, kv_pos),
-            value=features, attn_mask=kv_mask,
-            key_padding_mask=kv_padding_mask
-        )
-        queries = self.ln2(queries + self.dp2(qk_attn))
-        queries = self.ln3(queries + self.dp3(self.ffn(queries)))
-
-        outputs = [queries,]
-        if return_q_attn_weights:
-            outputs.append(q_attn_weights)
-        if return_qk_attn_weights:
-            outputs.append(qk_attn_weights)
-        return outputs
-
-class TransformerDecoder(nn.Module):
-
-    def __init__(self, decoder_layer, num_layers, return_intermediate=False):
-        super().__init__()
-        self.layers = nn.ModuleList([copy.deepcopy(decoder_layer) for i in range(num_layers)])
-        self.num_layers = num_layers
-        self.norm = nn.LayerNorm(decoder_layer.q_dim)
-        self.return_intermediate = return_intermediate
-
-        self._reset_parameters()
-
-    def _reset_parameters(self):
-        for p in self.parameters():
-            if p.dim() > 1:
-                nn.init.xavier_uniform_(p)
-
-    def forward(self, queries, features,
-            q_mask: Optional[Tensor] = None,
-            kv_mask: Optional[Tensor] = None,
-            q_padding_mask: Optional[Tensor] = None,
-            kv_padding_mask: Optional[Tensor] = None,
-            q_pos: Optional[Tensor] = None,
-            kv_pos: Optional[Tensor] = None,
-            return_q_attn_weights: Optional[bool] = False,
-            return_qk_attn_weights: Optional[bool] = False
-        ):
-        # Add support for zero layers
-        if self.num_layers == 0:
-            return queries.unsqueeze(0)
-
-        outputs = [queries,]
-        intermediate = []
-        q_attn_w = []
-        qk_attn_w = []
-        for layer in self.layers:
-            outputs = layer(
-                outputs[0], features,
-                q_mask=q_mask, kv_mask=kv_mask,
-                q_padding_mask=q_padding_mask,
-                kv_padding_mask=kv_padding_mask,
-                q_pos=q_pos, kv_pos=kv_pos,
-                return_q_attn_weights=return_q_attn_weights,
-                return_qk_attn_weights=return_qk_attn_weights
-            )
-            if self.return_intermediate:
-                intermediate.append(self.norm(outputs[0]))
-            if return_q_attn_weights:
-                q_attn_w.append(outputs[1])
-            if return_qk_attn_weights:
-                qk_attn_w.append(outputs[2])
-
-        if self.return_intermediate:
-            outputs = [torch.stack(intermediate),]
-        else:
-            outputs = [self.norm(outputs[0]).unsqueeze(0),]
-        if return_q_attn_weights:
-            q_attn_w = torch.stack(q_attn_w)
-            outputs.append(q_attn_w)
-        if return_qk_attn_weights:
-            qk_attn_w = torch.stack(qk_attn_w)
-            outputs.append(qk_attn_w)
-        return outputs
-
 class Permute(nn.Module):
     def __init__(self, dims: List[int]):
         super().__init__()
@@ -519,6 +341,15 @@ class UPT(nn.Module):
 
         return detections
 
+    def fetch_reference_points(self, boxes, paired_inds, image_sizes):
+        reference_points = []
+        for bx, p_inds, sz in zip(boxes, paired_inds, image_sizes):
+            h, w = sz
+            bx_c = torch.stack([bx[:, 0] / w, bx[:, 1] / h], dim=1)
+            p_bx_c = bx_c[p_inds].mean(1)
+            reference_points.append(p_bx_c)
+        return reference_points
+
     def forward(self,
         images: List[Tensor],
         triplet_cands: Optional[List[int]] = None,
@@ -566,7 +397,7 @@ class UPT(nn.Module):
 
         src, mask = features[-1].decompose()
         assert mask is not None
-        hs, memory = self.transformer(self.input_proj(src), mask, self.query_embed.weight, pos[-1])
+        hs, _ = self.transformer(self.input_proj(src), mask, self.query_embed.weight, pos[-1])
 
         outputs_class = self.class_embed(hs)
         outputs_coord = self.bbox_embed(hs).sigmoid()
@@ -609,32 +440,21 @@ class UPT(nn.Module):
         masks.append(mask_c6)
         pos.append(pos_c6)
 
-        # Compute keys/values for the decoder.
+        # Compute keys/values for the triplet decoder.
         memory, spatial_shapes, level_start_index, valid_ratios, mask_flatten = self.feature_head(srcs, masks, pos)
 
-        # TODO Decoder logic
-
-        src, mask = features[self.fusion_layer].decompose()
-        memory = self.feature_head(src)
-        b, h, w, c = memory.shape
-        memory = memory.reshape(b, h * w, c)
-        kv_p_m = mask.reshape(-1, 1, h * w)
-        kv_pos = pos[self.fusion_layer].permute(0, 2, 3, 1).reshape(b, h * w, 1, c)
-
-        output_queries = []
+        reference_points = self.fetch_reference_points(boxes, paired_inds, image_sizes)
+        # Run decoder per image, due to the disparity in query numbers.
+        ho_embeds = []
         for i, (ho_q, mem) in enumerate(zip(ho_queries, memory)):
-            output_queries.append(self.decoder(
-                ho_q.unsqueeze(1), mem.unsqueeze(1),
-                # q_padding_mask=q_padding_mask,
-                kv_padding_mask=kv_p_m[i],
-                kv_pos=kv_pos[i]
-            )[0].squeeze(dim=2))
-        mm_queries_collate = torch.cat(output_queries, dim=1)
-        # mm_queries_collate = torch.cat([
-        #     mm_q[q_p_m] for mm_q, q_p_m
-        #     in zip(padded_queries, q_padding_mask)
-        # ])
-        logits = self.binary_classifier(mm_queries_collate)
+            hs, _ = self.decoder(
+                ho_q[None], reference_points[i][None], mem[None],
+                spatial_shapes, level_start_index,
+                valid_ratios[i: i+1], None, mask_flatten[i: i+1]
+            )
+            ho_embeds.append(hs[:, 0])
+        ho_embeds = torch.cat(ho_embeds, dim=1)
+        logits = self.binary_classifier(ho_embeds)
 
         if self.training:
             labels = associate_with_ground_truth(

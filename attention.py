b6b3b5528a83fc45668d7c44b4ca35a4f5860da4
fred@gcr <frederic.zhang@anu.edu.au>
2023-03-14 00:57:52 +0000

    avoid averaging the attention weights

--- attention.py
+++ attention.py
@@ -389,6 +389,7 @@ def multi_head_attention_forward(query: Tensor,
     if need_weights:
         # average attention weights over heads
         attn_output_weights = attn_output_weights.view(bsz, num_heads, tgt_len, src_len)
-        return attn_output, attn_output_weights.sum(dim=1) / num_heads
+        # return attn_output, attn_output_weights.sum(dim=1) / num_heads
+        return attn_output, attn_output_weights
     else:
         return attn_output, None

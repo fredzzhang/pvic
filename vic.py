9ea5f2d115edad166a0834eff8580796cbffaee2
fred@gcr <frederic.zhang@anu.edu.au>
2023-03-27 06:53:29 +0000

    include forward method for advanced detr variants

--- vic.py
+++ vic.py
@@ -33,7 +33,8 @@ from ops import (
     compute_sinusoidal_pe
 )
 
-from detr.models import build_model
+from detr.models import build_model as build_base_detr
+from h_detr.models import build_model as build_advanced_detr
 from detr.models.position_encoding import PositionEmbeddingSine
 from detr.util.misc import NestedTensor, nested_tensor_from_tensor_list
 
@@ -196,6 +197,12 @@ class FeatureHead(nn.Module):
         x = self.layers(x)
         return x, mask
 
+def inverse_sigmoid(x, eps=1e-5):
+    x = x.clamp(min=0, max=1)
+    x1 = x.clamp(min=eps)
+    x2 = (1 - x).clamp(min=eps)
+    return torch.log(x1 / x2)
+
 class ViC(nn.Module):
     """Two-stage HOI detector with enhanced visual context"""
 
@@ -303,7 +310,93 @@ class ViC(nn.Module):
 
     @staticmethod
     def advanced_forward(ctx, samples: NestedTensor):
-        pass
+        if not isinstance(samples, NestedTensor):
+            samples = nested_tensor_from_tensor_list(samples)
+        features, pos = ctx.backbone(samples)
+
+        srcs = []
+        masks = []
+        for l, feat in enumerate(features):
+            src, mask = feat.decompose()
+            srcs.append(ctx.input_proj[l](src))
+            masks.append(mask)
+            assert mask is not None
+        if ctx.num_feature_levels > len(srcs):
+            _len_srcs = len(srcs)
+            for l in range(_len_srcs, ctx.num_feature_levels):
+                if l == _len_srcs:
+                    src = ctx.input_proj[l](features[-1].tensors)
+                else:
+                    src = ctx.input_proj[l](srcs[-1])
+                m = samples.mask
+                mask = F.interpolate(m[None].float(), size=src.shape[-2:]).to(
+                    torch.bool
+                )[0]
+                pos_l = ctx.backbone[1](NestedTensor(src, mask)).to(src.dtype)
+                srcs.append(src)
+                masks.append(mask)
+                pos.append(pos_l)
+
+        query_embeds = None
+        if not ctx.two_stage or ctx.mixed_selection:
+            query_embeds = ctx.query_embed.weight[0 : ctx.num_queries, :]
+
+        self_attn_mask = (
+            torch.zeros([ctx.num_queries, ctx.num_queries,]).bool().to(src.device)
+        )
+        self_attn_mask[ctx.num_queries_one2one :, 0 : ctx.num_queries_one2one,] = True
+        self_attn_mask[0 : ctx.num_queries_one2one, ctx.num_queries_one2one :,] = True
+
+        (
+            hs,
+            init_reference,
+            inter_references,
+            enc_outputs_class,
+            enc_outputs_coord_unact,
+        ) = ctx.transformer(srcs, masks, pos, query_embeds, self_attn_mask)
+
+        outputs_classes_one2one = []
+        outputs_coords_one2one = []
+        outputs_classes_one2many = []
+        outputs_coords_one2many = []
+        for lvl in range(hs.shape[0]):
+            if lvl == 0:
+                reference = init_reference
+            else:
+                reference = inter_references[lvl - 1]
+            reference = inverse_sigmoid(reference)
+            outputs_class = ctx.class_embed[lvl](hs[lvl])
+            tmp = ctx.bbox_embed[lvl](hs[lvl])
+            if reference.shape[-1] == 4:
+                tmp += reference
+            else:
+                assert reference.shape[-1] == 2
+                tmp[..., :2] += reference
+            outputs_coord = tmp.sigmoid()
+
+            outputs_classes_one2one.append(outputs_class[:, 0 : ctx.num_queries_one2one])
+            outputs_classes_one2many.append(outputs_class[:, ctx.num_queries_one2one :])
+            outputs_coords_one2one.append(outputs_coord[:, 0 : ctx.num_queries_one2one])
+            outputs_coords_one2many.append(outputs_coord[:, ctx.num_queries_one2one :])
+        outputs_classes_one2one = torch.stack(outputs_classes_one2one)
+        outputs_coords_one2one = torch.stack(outputs_coords_one2one)
+        outputs_classes_one2many = torch.stack(outputs_classes_one2many)
+        outputs_coords_one2many = torch.stack(outputs_coords_one2many)
+
+        out = {
+            "pred_logits": outputs_classes_one2one[-1],
+            "pred_boxes": outputs_coords_one2one[-1],
+            "pred_logits_one2many": outputs_classes_one2many[-1],
+            "pred_boxes_one2many": outputs_coords_one2many[-1],
+        }
+
+        if ctx.two_stage:
+            enc_outputs_coord = enc_outputs_coord_unact.sigmoid()
+            out["enc_outputs"] = {
+                "pred_logits": enc_outputs_class,
+                "pred_boxes": enc_outputs_coord,
+            }
+        return out, hs[-1], features
 
     def forward(self,
         images: List[Tensor],
@@ -354,9 +447,11 @@ class ViC(nn.Module):
         )
         boxes = [r['boxes'] for r in region_props]
         # Produce human-object pairs.
-        ho_queries, paired_inds, prior_scores, object_types, positional_embeds = self.ho_matcher(
-            region_props, image_sizes
-        )
+        (
+            ho_queries,
+            paired_inds, prior_scores,
+            object_types, positional_embeds
+        ) = self.ho_matcher(region_props, image_sizes)
         # Compute keys/values for triplet decoder.
         memory, mask = self.feature_head(features)
         b, h, w, c = memory.shape
@@ -392,7 +487,11 @@ class ViC(nn.Module):
         return detections
 
 def build_detector(args, obj_to_verb):
-    detr, _, postprocessors = build_model(args)
+    if args.detector == "base":
+        detr, _, postprocessors = build_base_detr(args)
+    elif args.detector == "advanced":
+        detr, _, postprocessors = build_advanced_detr(args)
+
     if os.path.exists(args.pretrained):
         if dist.is_initialized():
             print(f"Rank {dist.get_rank()}: Load weights for the object detector from {args.pretrained}")
